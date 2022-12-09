e488278200a18df0106167dec9fb138ad8f712ff
Ahmet Yildirim <ahmet.yildirim@iln.uio.no>
2022-12-09 11:09:38 +0100

    commit

--- run_in_conda.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#condapath=$(conda info | grep -i 'base environment'|cut -d ":" -f 2 | cut -d " " -f 2)
-condapath=/storage/ahmetyi/anaconda3
-source $condapath/etc/profile.d/conda.sh
-conda activate transformerFL
-"$@"
-conda deactivate
