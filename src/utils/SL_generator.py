0f9e56a332714e31bb569af034948ac33420cc7a
vumaiha <maihavu@gmail.com>
2023-01-31 14:50:45 +0100

    fixed SL_generator.py so now it imports torch

--- src/utils/SL_generator.py
+++ src/utils/SL_generator.py
@@ -1,4 +1,10 @@
+import sys
 import numpy as np
+import torch
+from collections import defaultdict
+import ipdb as pdb
+import random
+import textwrap
 
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 seed = np.random.randint (0,100000000)
